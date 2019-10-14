ca41851373dfab366dd2ce33460496f01113e1cb
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-10-14 19:49:44 +0000

    initial commiot

---
+++ Members.js
@@ -0,0 +1,29 @@
+//Model
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Plain',
+        email: 'Jane@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Greg Gregerson',
+        email: 'gg@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 4,
+        name: 'Dorian Grey',
+        email: 'dgrey@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
